8af3a7d6237a5db6e6eb160aa2c024fc0ec248d5
VairaSimone <158765247+VairaSimone@users.noreply.github.com>
2024-09-26 19:33:13 +0200

    v2

--- backend/controllers/AuthRoute_controller.js
+++ backend/controllers/AuthRoute_controller.js
@@ -30,7 +30,7 @@ export const login = async (req, res) => {
 
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: true,
+            secure: false,
             sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
