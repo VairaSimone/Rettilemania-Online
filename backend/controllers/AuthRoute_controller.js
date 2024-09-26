3b236a195095aec3a98233a1dee4f95aafc0975b
VairaSimone <158765247+VairaSimone@users.noreply.github.com>
2024-09-26 19:08:10 +0200

    V2

--- backend/controllers/AuthRoute_controller.js
+++ backend/controllers/AuthRoute_controller.js
@@ -30,8 +30,8 @@ export const login = async (req, res) => {
 
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: none,
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
